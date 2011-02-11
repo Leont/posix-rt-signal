af3856b66068c6a74fab29e4b480cd81acb8098d
Leon Timmermans <fawaka@gmail.com>
2011-02-11 19:25:44 +0100

    Various documentation fixes

--- lib/Signal/More.pm
+++ lib/Signal/More.pm
@@ -18,13 +18,12 @@ __END__
 
 =head1 SYNOPSIS
 
- use Signal::More qw//;
- use Signal::Mask '%SIG_MASK';
- use POSIX 'SIGUSR1';
+ use Signal::More qw/sigqueue sigwait/;
+ use Signal::Mask;
  
- $SIG_MASK{USR1}++;
- sigqueue($$, SIGUSR1);
- sigwait(POSIX::SigSet->new(SIGUSR1));
+ $Signal::Mask{USR1}++;
+ sigqueue($$, 'USR1');
+ sigwait('USR1');
 
 =func sigqueue($pid, $sig, $value = 0)
 
@@ -32,7 +31,7 @@ Queue a signal $sig to process $pid, optionally with the additional argument $va
 
 =func sigwait($signals, $timeout = undef)
 
-Wait for a signal in $signals to arrive and return it. The signal handler (if any) will not be called. Unlike signal handlers it is not affected by signal masks, in fact you are expected to mask signals you're waiting for. $signals must either be a POSIX::SigSet object, a signal number or a signal name. If $timeout is specified, it indicates the maximal time the thread is suspended in fractional seconds; if no signal is received it returns an empty list, or in void context an exception. If $timeout is not defined it may wait indefititely until a signal arrives.
+Wait for a signal in $signals to arrive and return it. The signal handler (if any) will not be called. Unlike signal handlers it is not affected by signal masks, in fact you are expected to mask signals you're waiting for. $signals must either be a POSIX::SigSet object, a signal number or a signal name. If $timeout is specified, it indicates the maximal time the thread is suspended in fractional seconds; if no signal is received it returns an empty list, or in void context an exception. If $timeout is not defined it may wait indefinitely until a signal arrives.
 
 =head1 SEE ALSO
 
